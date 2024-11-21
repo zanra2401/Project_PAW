3da63664ebba339898ba6678ac47ae4217a81852
Nendra12 <mahardhika658@gmail.com>
2024-11-21 19:37:07 +0700

    test

--- src/views/Components/NavBar.php
+++ src/views/Components/NavBar.php
@@ -2,6 +2,6 @@
     <a href="/project_paw">
         <img src="<?= ASSETS ?>image/logo.png" class="w-12" alt="">
     </a>
-    <h1 class="font-medium text-base-color text-w text-2xl ml-2 font-Roboto-medium">Cari Kost</h1>
+    <h1 class="font-medium text-base-color text-w text-2xl ml-2 font-Roboto-medium" style="color: red;">Cari Kost</h1>
     <button class="ml-auto border-2 border-base-color p-1 pl-2 pr-2 rounded-md text-base-color font-Roboto-bold hover:text-white  hover:bg-base-color">Masuk</button>
 </nav>
