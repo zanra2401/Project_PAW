4ca077686d5615ba0c6949e453b8ad2c10d8952d
zanra2401 <grubgrex@gmail.com>
2024-12-12 16:07:50 +0700

    update

--- src/views/Admin/logPengumuman.php
+++ src/views/Admin/logPengumuman.php
@@ -1,5 +1,5 @@
 <?php require "./views/Components/Head.php" ?>
-<body class="h-screen flex overflow-hidden font-Roboto-normal">
+<body class="h-screen flex overflow-hidden">
     <style>
         /* width */
         ::-webkit-scrollbar {
@@ -23,95 +23,102 @@
     </style>
     <?php require "./views/Components/sidebarAdmin.php"  ?>
     <main class="p-5 w-full min-h-screen flex flex-col gap-3">
+        <!-- LOKASI -->
         <span class="mb-3 inline-block text-gray-500"> <i class="fas fa-microphone"></i> Pengumuman <i class="fas fa-chevron-right"></i> <i class="fas fa-sticky-note"></i> Log <i class="fas fa-chevron-right"></i></span>
-        <div class="space-y-3">
-            <input type="text" placeholder="Cari Pengumuman" p-0 class="w-full font-medium rounded-sm border-2 border-gray-500 focus:outline-none p-1 shaodw-sm shadow-gray-700">
+        <div class="space-y-3 ">
+            <div class="h-fit w-full flex gap-2 items-center p-1 rounded-sm border-2 border-gray-500 shaodwmdm shadow-gray-700">
+                <i class="fas fa-search text-gray-500"></i>
+                <input type="text" placeholder="Cari Pengumuman" class="w-full focus:outline-none font-medium" >
+            </div> 
+
             <div class="border-b-2 space-x-3 border-gray-500 font-Roboto-medium">
-                 <span class="hover:cursor-pointer h-full px-2 py-1 <?= ($data['active-menu'] == 'semua') ? 'text-white  rounded-sm bg-gray-900' : '' ?>">
+                <a href="/<?= PROJECT_NAME ?>/admin/logpengumuman/semua" class="hover:cursor-pointer h-full px-2 py-1 <?= ($data['active-menu'] == 'semua') ? 'text-white  rounded-sm bg-warna-second' : '' ?>">
                     Semua
-                 </span>
-                 <span class="hover:cursor-pointer h-full px-2 py-1 <?= ($data['active-menu'] == 'pencari') ? 'text-white  rounded-sm bg-gray-900' : '' ?>">
+                </a>
+                <a href="/<?= PROJECT_NAME ?>/admin/logpengumuman/pencari" class="hover:cursor-pointer h-full px-2 py-1 <?= ($data['active-menu'] == 'pencari') ? 'text-white  rounded-sm bg-warna-second' : '' ?>">
                     Pencari Kost
-                 </span>
-                 <span class="hover:cursor-pointer h-full px-2 py-1 <?= ($data['active-menu'] == 'pemilik') ? 'text-white  rounded-sm bg-gray-900' : '' ?>">
+                </a>
+                <a href="/<?= PROJECT_NAME ?>/admin/logpengumuman/pemilik" class="hover:cursor-pointer h-full px-2 py-1 <?= ($data['active-menu'] == 'pemilik') ? 'text-white  rounded-sm bg-warna-second' : '' ?>">
                     Pemilik Kost
-                 </span>
+                </a>
             </div>
         </div>
         <div class="h-full space-y-8 overflow-y-scroll">
             <div class="mt-5 relative">
-                <span class="absolute px-3 py-1 bg-white font-Roboto-bold border-2 border-gray-500 rounded-sm -translate-y-1/2">
+                <span class="absolute px-3 py-1 text-gray-700 bg-white font-Roboto-bold border-2 border-gray-300 rounded-sm -translate-y-1/2">
                     Kami, 27 Agustus 2005
                 </span>
-                <div class="px-2 py-6 gap-3 border-t-2 border-gray-500 flex items-center">
+                <div class="px-2 py-6 gap-3 border-t-2 border-gray-300 flex items-center">
                     <span>
-                        <i class="far fa-envelope text-[40px] text-gray-700"></i>
+                        <i class="far fa-envelope text-[40px] text-warna-third"></i>
                     </span>
                     <span class="translate-y-1/4">
-                        <h3 class="font-Roboto-bold text-gray-700">Pembaruan Baru</h3>
+                        <h3 class="font-Roboto-bold text-gray-500">Pembaruan Baru</h3>
                         <a href="" class="ml-auto translate-y-1/2 inline-block text-warna-third hover:underline font-Roboto-bold">Detail</a>
                     </span>
-                    <span class="ml-auto font-Roboto-bold text-gray-700">01:00 PM</span>
+                    <span class="ml-auto font-Roboto-bold text-gray-500">01:00 PM</span>
                 </div>
             </div>
             <div class="mt-5 relative">
-                <span class="absolute px-3 py-1 bg-white font-Roboto-bold border-2 border-gray-500 rounded-sm -translate-y-1/2">
+                <span class="absolute px-3 py-1 text-gray-700 bg-white font-Roboto-bold border-2 border-gray-300 rounded-sm -translate-y-1/2">
                     Kami, 27 Agustus 2005
                 </span>
-                <div class="px-2 py-6 gap-3 border-t-2 border-gray-500 flex items-center">
+                <div class="px-2 py-6 gap-3 border-t-2 border-gray-300 flex items-center">
                     <span>
-                        <i class="far fa-envelope text-[40px] text-gray-700"></i>
+                        <i class="far fa-envelope text-[40px] text-warna-third"></i>
                     </span>
                     <span class="translate-y-1/4">
-                        <h3 class="font-Roboto-bold text-gray-700">Pembaruan Baru</h3>
+                        <h3 class="font-Roboto-bold text-gray-500">Pembaruan Baru</h3>
                         <a href="" class="ml-auto translate-y-1/2 inline-block text-warna-third hover:underline font-Roboto-bold">Detail</a>
                     </span>
-                    <span class="ml-auto font-Roboto-bold text-gray-700">01:00 PM</span>
+                    <span class="ml-auto font-Roboto-bold text-gray-500">01:00 PM</span>
                 </div>
             </div>
             <div class="mt-5 relative">
-                <span class="absolute px-3 py-1 bg-white font-Roboto-bold border-2 border-gray-500 rounded-sm -translate-y-1/2">
+                <span class="absolute px-3 py-1 text-gray-700 bg-white font-Roboto-bold border-2 border-gray-300 rounded-sm -translate-y-1/2">
                     Kami, 27 Agustus 2005
                 </span>
-                <div class="px-2 py-6 gap-3 border-t-2 border-gray-500 flex items-center">
+                <div class="px-2 py-6 gap-3 border-t-2 border-gray-300 flex items-center">
                     <span>
-                        <i class="far fa-envelope text-[40px] text-gray-700"></i>
+                        <i class="far fa-envelope text-[40px] text-warna-third"></i>
                     </span>
                     <span class="translate-y-1/4">
-                        <h3 class="font-Roboto-bold text-gray-700">Pembaruan Baru</h3>
+                        <h3 class="font-Roboto-bold text-gray-500">Pembaruan Baru</h3>
                         <a href="" class="ml-auto translate-y-1/2 inline-block text-warna-third hover:underline font-Roboto-bold">Detail</a>
                     </span>
-                    <span class="ml-auto font-Roboto-bold text-gray-700">01:00 PM</span>
+                    <span class="ml-auto font-Roboto-bold text-gray-500">01:00 PM</span>
                 </div>
             </div>
+
             <div class="mt-5 relative">
-                <span class="absolute px-3 py-1 bg-white font-Roboto-bold border-2 border-gray-500 rounded-sm -translate-y-1/2">
+                <span class="absolute px-3 py-1 text-gray-700 bg-white font-Roboto-bold border-2 border-gray-300 rounded-sm -translate-y-1/2">
                     Kami, 27 Agustus 2005
                 </span>
-                <div class="px-2 py-6 gap-3 border-t-2 border-gray-500 flex items-center">
+                <div class="px-2 py-6 gap-3 border-t-2 border-gray-300 flex items-center">
                     <span>
-                        <i class="far fa-envelope text-[40px] text-gray-700"></i>
+                        <i class="far fa-envelope text-[40px] text-warna-third"></i>
                     </span>
                     <span class="translate-y-1/4">
-                        <h3 class="font-Roboto-bold text-gray-700">Pembaruan Baru</h3>
+                        <h3 class="font-Roboto-bold text-gray-500">Pembaruan Baru</h3>
                         <a href="" class="ml-auto translate-y-1/2 inline-block text-warna-third hover:underline font-Roboto-bold">Detail</a>
                     </span>
-                    <span class="ml-auto font-Roboto-bold text-gray-700">01:00 PM</span>
+                    <span class="ml-auto font-Roboto-bold text-gray-500">01:00 PM</span>
                 </div>
             </div>
+            
             <div class="mt-5 relative">
-                <span class="absolute px-3 py-1 bg-white font-Roboto-bold border-2 border-gray-500 rounded-sm -translate-y-1/2">
+                <span class="absolute px-3 py-1 text-gray-700 bg-white font-Roboto-bold border-2 border-gray-300 rounded-sm -translate-y-1/2">
                     Kami, 27 Agustus 2005
                 </span>
-                <div class="px-2 py-6 gap-3 border-t-2 border-gray-500 flex items-center">
+                <div class="px-2 py-6 gap-3 border-t-2 border-gray-300 flex items-center">
                     <span>
-                        <i class="far fa-envelope text-[40px] text-gray-700"></i>
+                        <i class="far fa-envelope text-[40px] text-warna-third"></i>
                     </span>
                     <span class="translate-y-1/4">
-                        <h3 class="font-Roboto-bold text-gray-700">Pembaruan Baru</h3>
+                        <h3 class="font-Roboto-bold text-gray-500">Pembaruan Baru</h3>
                         <a href="" class="ml-auto translate-y-1/2 inline-block text-warna-third hover:underline font-Roboto-bold">Detail</a>
                     </span>
-                    <span class="ml-auto font-Roboto-bold text-gray-700">01:00 PM</span>
+                    <span class="ml-auto font-Roboto-bold text-gray-500">01:00 PM</span>
                 </div>
             </div>
         </div>
